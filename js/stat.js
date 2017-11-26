3d6705a7bddadef7790e9c47ca57287c4ea32e56
Edwardo1988 <edwardo@edwardo-osx.local>
2017-11-26 16:35:02 +0300

    Подготовил гистограмму со статистикой игроков

---
+++ js/stat.js
@@ -0,0 +1,58 @@
+window.renderStatistics = function(ctx, names, times) {
+
+  var names = ['Вы', 'Катя', 'Петя', 'Оля'];
+  var times = [350.5, 120, 110, 110];
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, 420, 270);
+  
+  ctx.fillStyle = 'white';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+  
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+
+
+var getMaxElement = function(){
+  var max = 0;
+  var maxIndex = 0;
+  for (var i = 0; i < times.length, i++;){
+  var time = arr[i];
+  if (time > max){
+    max = time;
+    maxIndex = i;
+    }
+  }
+  return max;
+}
+
+
+var histogramMaxElementY = times[0]; 
+var histogramHeight = 150;
+var columnWidth = 40;
+var columnInterval = 50;
+var myColour = 'rgba(255, 0, 0, 1)';
+var textColour = 'rgba(0, 0, 0, 1)';
+var histogramX = 120;
+var histogramY = 250;
+
+for (var i = 0; i <= times.length - 1; i++){
+  ctx.globalAlpha = Math.random();
+  var otherPlayersColour = 'rgb(0, 0, 255)';
+  times[i] = Math.round(times[i]);
+  if (names[i] == 'Вы'){
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = myColour;}
+  else {ctx.fillStyle = otherPlayersColour;}
+  ctx.fillRect(histogramX + columnWidth * i + columnInterval * i, histogramY, columnWidth, times[i] * histogramHeight / - histogramMaxElementY);
+  ctx.fillStyle = textColour;
+  ctx.fillText(names[i], histogramX + columnWidth * i + columnInterval * i, histogramY + 20);
+  ctx.fillText(times[i], histogramX + columnWidth * i + columnInterval * i, - (times[i] * histogramHeight / histogramMaxElementY) + 240);
+  }
+}
